d8f81871d7e3b03215ea3d5e8cb78e791efc3b02
Miriam <rezakmiriam@gmail.com>
2022-03-25 22:49:10 +0100

    cyklus for

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 // Použij výpis do konzole prohlížeče pomocí console.log('text')
 // Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
 
-
 // První příklad - vypiš vzestupně čísla od 0 do 10
 console.log('První příklad - vzestupná řada 0 - 10');
 
 //TADY NAPIS CYKLUS
 
-console.log('-------------------');
-
-
+for (let x = 0; x <= 10; x++) {
+  console.log(x);
+}
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
@@ -20,8 +19,6 @@ console.log('Druhý příklad - rozdíl mezi <= a <');
 //TADY NAPIS CYKLUS
 
 console.log('-------------------');
-
-
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
