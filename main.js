71abcdd870ff853700a5870e1e7cb7ebfa989c63
Miriam <rezakmiriam@gmail.com>
2022-03-25 22:41:53 +0100

    Zmena na while cyklus

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 // Použij výpis do konzole prohlížeče pomocí console.log('text')
 // Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
 
-
 // První příklad - vypiš vzestupně čísla od 0 do 10
 console.log('První příklad - vzestupná řada 0 - 10');
 
-//TADY NAPIS CYKLUS
+//TADY NAPIS CYKLUS WHILE
+let a = 0;
 
-console.log('-------------------');
-
-
+while (a <= 10) {
+  console.log(a);
+  a++;
+}
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
@@ -17,15 +18,23 @@ console.log('-------------------');
 
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
-//TADY NAPIS CYKLUS
+//TADY NAPIS CYKLUS WHILE
 
-console.log('-------------------');
+let b = 0;
 
-
+while (b < 10) {
+  console.log(b);
+  b++;
+}
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
-//TADY NAPIS CYKLUS
+//TADY NAPIS CYKLUS WHILE
 
-console.log('-------------------');
+let c = 10;
+
+while (c >= 0) {
+  console.log(c);
+  c--;
+}
